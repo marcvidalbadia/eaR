ae32a27b2d1806539579dba3d02da4812f3d777e
m-vidal <54290040+m-vidal@users.noreply.github.com>
2021-09-24 21:15:04 +0200

    Update AdaptLevel.R

--- R/AdaptLevel.R
+++ R/AdaptLevel.R
@@ -1,5 +1,5 @@
-AdaptLevel <- function(inSignal, indB)
-{
+AdaptLevel <- function(inSignal, 
+                       indB) {
   theRMS <- sqrt(sum(inSignal^2)/length(inSignal))
   theFactor <- 10^(indB/20 - log10(theRMS))
   outSignaldB <- inSignal*theFactor
