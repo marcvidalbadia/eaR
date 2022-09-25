2fa3df129120c505e656c71d814725f546e9e365
m-vidal <54290040+m-vidal@users.noreply.github.com>
2022-09-25 14:00:31 +0200

    Add files via upload

---
+++ R/LeakyIntegration.R
@@ -0,0 +1,28 @@
+LeakyIntegration <- function(inSignal,
+                             inSampleFreq,
+                             inHalfDecayTime = 0.1,
+                             inEnlargement = 0) {
+  if (inEnlargement == -1) {
+    inEnlargement <- 2*inHalfDecayTime
+  }
+
+  if (inHalfDecayTime != 0) {
+    integrator <- 2^(-1/(inSampleFreq*inHalfDecayTime))
+    } else {
+    integrator <- 0
+  }
+
+  #Fits a new matrix for the enlargement
+  Ae <- cbind(inSignal,
+                  matrix(0L, nrow = nrow(inSignal),
+                         ncol = round(inSampleFreq*inEnlargement)))
+
+  outLeakyIntegration <- Ae*0 #deposit results
+
+  outLeakyIntegration[,1] <- Ae[,1]
+  for (i in 2:ncol(Ae)) {
+    outLeakyIntegration[,i] <- (outLeakyIntegration[,i-1]*integrator) + Ae[,i]
+  }
+
+  return(outLeakyIntegration)
+}
